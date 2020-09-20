dfb778c06b4d6c80d9a64b6540d3ebcef05713dc
Lumos393 <64517124+Lumos393@users.noreply.github.com>
2020-09-20 14:48:09 -0400

    Update glassdoor_search.py

--- glassdoor_search.py
+++ glassdoor_search.py
@@ -4,9 +4,8 @@ import requests
 from bs4 import BeautifulSoup
 import lxml
 
-url = 'https://www.glassdoor.com/Job/suwanee-human-resources-jobs-SRCH_IL.0,7_IC1155695_KE8,23.htm?'
-
-reqs = requests.get(url)
+reqs = requests.get(
+    'http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=120&t.k=fz6JLNDfgVs&action=employers&q=pharmaceuticals&userip=192.168.43.42&useragent=Mozilla/%2F4.0')
 soup = BeautifulSoup(reqs.text, 'lxml')
 
-print(soup.find_all('span'))
+print(reqs)
