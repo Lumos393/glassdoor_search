e79950b0a389b03fdc3ace057a0efdc72c65a599
Lumos393 <64517124+Lumos393@users.noreply.github.com>
2020-09-22 13:26:37 -0400

    Update glassdoor_search.py

--- glassdoor_search.py
+++ glassdoor_search.py
@@ -1,29 +1,23 @@
-import sys
-import os
-import requests
+from re import findall
 from bs4 import BeautifulSoup
-import lxml
 from urllib.request import urlopen, Request
+from bs4 import element
 from requests.api import request
 
 user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
 
-#url = 'https://www.glassdoor.com/Job/suwanee-human-resources-jobs-SRCH_IL.0,7_IC1155695_KE8,23.htm?'
-url = 'https://www.wikipedia.org/'
+url = 'https://www.glassdoor.com/Job/suwanee-human-resources-jobs-SRCH_IL.0,7_IC1155695_KE8,23.htm?'
+headers = {'User-Agent':user_agent}
+req = Request(url, None, headers)
 
-headers = {'User-Agent':user_agent,}
-request = Request(url, None, headers)
-page = urlopen(url)
-
-data = page.read()
-print(data)
+page = urlopen(req)
 
 html_bytes = page.read()
-html = html_bytes.decode('utf-8')
+soup = BeautifulSoup(html_bytes, 'html.parser')
+#strhtm = soup.prettify()
+mysoup = soup.findAll('li', {'class': 'jl react-job-listing gdGrid'})
 
-print(html)
+mysoupy = soup.attrs.fromkeys(mysoup)
 
-reqs = requests.get(url)
-soup = BeautifulSoup(reqs.text, 'lxml')
-
-print(soup.find_all('span'))
+for x in mysoup:
+    print(x, '\n')
