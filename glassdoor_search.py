05fa01a2a2b8dbdb5cb1b5de814425cbce32ca08
Lumos393 <64517124+Lumos393@users.noreply.github.com>
2020-09-23 15:16:23 -0400

    minor add

--- glassdoor_search.py
+++ glassdoor_search.py
@@ -20,4 +20,6 @@ for li in soup.findAll('li', {'class': 'jl react-job-listing gdGrid'}):
     soupy_mess.append(li['data-normalize-job-title'])
     soupy_mess.append(soup.find('span'))
 
+for 
+
 print(soupy_mess)
